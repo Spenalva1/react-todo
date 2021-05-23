53b221aff45fcc8a5c110a7da22868575d2a6c86
spenalva1 <penalvasantiagogm@gmail.com>
2021-05-23 13:53:16 -0300

    item border modified

--- react-todo/src/components/TodoItem.js
+++ react-todo/src/components/TodoItem.js
@@ -6,7 +6,7 @@ import CheckButton from './CheckButton';
 const TodoItem = ({todo, toggleTodoCheck, deleteTodo, mobileDesign}) => {
     const {darkMode} = useDarkMode();
     return (
-        <TodoItemStyles className={`flex items-center p-4 ${darkMode ? 'bg-darkVeryDarkDesaturatedBlue' : 'bg-white'} overflow-hidden w-full relative border-b border-gray-50`}>
+        <TodoItemStyles className={`flex items-center p-4 ${darkMode ? 'bg-darkVeryDarkDesaturatedBlue' : 'bg-white'} overflow-hidden w-full relative border-b`}>
             <CheckButton check={todo.check} toggleTodoCheck={() => toggleTodoCheck(todo.id)}/>
             <p className={`${darkMode ? 'text-white' : 'text-black'} flex-1 px-8 ${todo.check ? 'line-through opacity-40' : ''}`}>{todo.name}</p>
             <img onClick={() => deleteTodo(todo.id)} width="30px" height="30px" className={`${!mobileDesign ? 'desktop' : ''} cursor-pointer m-4 p-3`} src={crossIcon} alt="check" />
